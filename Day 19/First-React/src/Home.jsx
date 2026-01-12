5e3f21afdb7b25dd67854300a49a591720c7a8c9
sagar9rout <supritsagrrout@gmail.com>
2026-01-12 23:49:59 +0530

    react

---
+++ Day 19/First-React/src/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HeroPage from './HeroPage'
+
+function Home() {
+  return (
+    <div>This is Home Page
+       <HeroPage/>
+    </div>
+
+  )
+}
+
+export default Home
